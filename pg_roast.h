3f7fe87e0ca61cf27c6d7d0f8485b85dd5f4b8dd
Samir Ketema <6003000+samirketema@users.noreply.github.com>
2026-04-21 10:10:30 -0700

    fix: use PGDLLEXPORT for debian make

--- pg_roast.h
+++ pg_roast.h
@@ -8,6 +8,6 @@ extern int   pg_roast_interval_seconds;
 extern bool  pg_roast_auto_audit;
 extern char *pg_roast_database;
 
-extern void pg_roast_bgw_main(Datum main_arg);
+extern PGDLLEXPORT void pg_roast_bgw_main(Datum main_arg);
 
 #endif /* PG_ROAST_H */
